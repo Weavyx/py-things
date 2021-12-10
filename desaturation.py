2df333e5a0477e79a3eb4cc7d22657235c5648b0
Weavyx <95550080+Weavyx@users.noreply.github.com>
2021-12-10 20:13:57 +0100

    Update desaturation.py

--- desaturation.py
+++ desaturation.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
-def desaturation(pngname):
-    image1 = Image.open(pngname)
+def desaturation(pngName):
+    image1 = Image.open(pngName)
     x, y = image1.size
     for xpixel in range (x):
         for ypixel in range (y) :
@@ -11,11 +11,9 @@ def desaturation(pngname):
             else:
                 image1.putpixel((xpixel,ypixel),(0,0,0))         
         #print(xpixel/x*100, '%')
-    newname = 'new' + pngname
+    newname = 'new' + pngName
     image1.save(newname)
     image1.close()
 
-# 1.9193007946014404 seconds on a 731 Ko png png
+# 1.9193007946014404 seconds on a 731 Ko png
 desaturation('wolf.png')
-
-
